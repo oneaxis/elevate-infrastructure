357280b597720d1f35db4bd611074c0ba1450602
Timo Weber <timo_weber@outlook.com>
2026-09-10 21:00:35 +0200

    fix: WIF provider entity format for auth action; bump GitHub Actions to current majors (Node 24)

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -4,8 +4,8 @@ output "gateway_url" {
 }
 
 output "workload_identity_provider" {
-  description = "Fully qualified WIF provider path. Set as the WIF_PROVIDER GitHub Actions variable."
-  value       = "https://iam.googleapis.com/projects/${data.google_project.this.number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github.workload_identity_pool_id}/providers/${google_iam_workload_identity_pool_provider.github.workload_identity_pool_provider_id}"
+  description = "WIF provider entity name. Set as the WIF_PROVIDER GitHub Actions variable (google-github-actions/auth expects the //iam.googleapis.com/... resource-name form)."
+  value       = "//iam.googleapis.com/projects/${data.google_project.this.number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github.workload_identity_pool_id}/providers/${google_iam_workload_identity_pool_provider.github.workload_identity_pool_provider_id}"
 }
 
 output "deployer_service_account" {
